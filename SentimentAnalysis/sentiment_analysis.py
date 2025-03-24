02c7b5189cbd631bd6ecc4116e6e220371bc0c74
avastamin <r_amin105@yahoo.com>
2025-03-24 08:04:47 -0400

    error handling

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -9,7 +9,13 @@ def sentiment_analyzer(text_to_analyse):
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
     
     formatted_response = json.loads(response.text)
-    label = formatted_response['documentSentiment']['label']
-    score = formatted_response['documentSentiment']['score']
+    #if the response status code is 200, extract the label and score from the response
+    if response.status_code == 200:
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    elif response.status_code == 500:
+        label = None
+        score = None    
 
+    # Return the label and score in a dictionary
     return {'label': label, 'score': score}
